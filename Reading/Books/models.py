ed09da19c30d549e9ea041f6ca8cc82d5b228d41
Faris <fcosic780@gmail.com>
2022-07-14 14:54:52 +0200

    Added user_book model

--- Reading/Books/models.py
+++ Reading/Books/models.py
@@ -5,3 +5,10 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     genres = models.JSONField(default=list)
+
+class user_book(models.Model):
+    user_id = models.ForeignKey(User, related_name="user", on_delete=models.CASCADE)
+    book_isbn = models.IntegerField()
+    to_read = models.BooleanField(default=False)
+    is_read = models.BooleanField(default=False)
+    is_reading = models.BooleanField(default=False)
